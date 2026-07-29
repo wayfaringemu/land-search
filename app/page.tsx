eb3faf1bb0696897d9648f42bd2759f6cbcb9e21
wayfaringemu <rykowalspt@gmail.com>
2026-07-29 08:39:58 -0400

    Fix TypeScript maxLength type error

--- app/page.tsx
+++ app/page.tsx
@@ -96,7 +96,7 @@ export default function Home() {
                     value={state}
                     onChange={(e) => setState(e.target.value.toUpperCase())}
                     placeholder="e.g., TX"
-                    maxLength="2"
+                    maxLength={2}
                     className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500"
                   />
                 </div>
